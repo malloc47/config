4d344912dbe5a1690bd81fb70aa177ee4b048d6f
Jarrell Waggoner <jwaggoner@groupon.com>
2014-05-30 15:44:04 -0500

    skeleton profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user [{:plugins [[lein-kibit "0.0.8"]
+                   [jonase/eastwood "0.0.2"]
+                   [lein-release "1.0.5"]
+                   [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}]}
