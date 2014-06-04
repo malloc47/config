bfc2f502698e4c9d63967e69bbeac7ba9e96251a
Jarrell Waggoner <jwaggoner@groupon.com>
2014-06-03 22:31:22 -0500

    rename to package-manager and partial refactor (incomplete)

---
+++ .emacs.d/config/package-manager.el
@@ -0,0 +1,57 @@
+(provide 'package-manager)
+
+(require 'package)
+;; (require 'cl)
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/"))
+
+(defvar package-manager-refreshed nil)
+
+(defun package-manager-refresh-once ()
+  (when (not package-manager-refreshed)
+    (setq package-manager-refreshed t)
+    (package-refresh-contents)))
+
+(defun ensure-packages-installed (&rest packages)
+  (dolist (package packages)
+    (when (not (package-installed-p package))
+      (package-manager-refresh-once)
+      (package-install package))))
+
+(package-initialize)
+
+(setq package-enable-at-startup nil)
+
+;; ;; Borrowed from prelude
+
+;; (defvar required-packages
+;;   '(clojure-mode
+;;     cider
+;;     align-cljlet
+;;     paredit
+;;     markdown-mode
+;;     magit
+;;     find-file-in-project
+;;     ido-ubiquitous
+;;     solarized-theme
+;;     zenburn-theme
+;;     haskell-mode)
+;;   "Custom package list installed on launch")
+
+;; (defun all-packages-installed-p ()
+;;   (loop for p in required-packages
+;;         when (not (package-installed-p p)) do (return nil)
+;;         finally (return t)))
+
+;; (unless (all-packages-installed-p)
+;;   (message "%s" "Refreshing package database...")
+;;   (package-refresh-contents)
+;;   (message "%s" " done.")
+;;   (dolist (p required-packages)
+;;     (when (not (package-installed-p p))
+;;       (package-install p))))
