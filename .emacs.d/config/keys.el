4cafe3ebee530fc07555febb8c5c1ed8432e4d72
Jarrell Waggoner <jwaggoner@groupon.com>
2014-06-19 11:49:45 -0500

    I've wanted these keybindings for a while

--- .emacs.d/config/keys.el
+++ .emacs.d/config/keys.el
@@ -8,3 +8,21 @@
 (global-set-key (kbd "C-x C-S-f") 'find-file-in-project)
 
 (global-set-key (kbd "C-x C-g") 'magit-status)
+
+(global-set-key (kbd "C-c C-g t") (function
+				   (lambda ()
+				     "Go to top of page."
+				     (interactive)
+				     (move-to-window-line 0))))
+
+(global-set-key (kbd "C-c C-g b") (function
+				   (lambda ()
+				     "Go to bottom of page."
+				     (interactive)
+				     (move-to-window-line -1))))
+
+(global-set-key (kbd "C-c C-g m") (function
+				   (lambda ()
+				     "Go to middle of page."
+				     (interactive)
+				     (move-to-window-line (/ (window-height) 2)))))
