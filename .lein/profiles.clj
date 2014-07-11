dfee6e157d1e4ed48b32a7551f2ddd390263dbd8
Jarrell Waggoner <jwaggoner@groupon.com>
2014-07-11 11:39:07 -0500

    get rid of some linters for now

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,1 @@
-{:user [{:plugins [[lein-kibit "0.0.8"]
-                   [jonase/eastwood "0.0.2"]
-                   [lein-release "1.0.5"]
-                   [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}]}
+{:user [{:plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]}]}
