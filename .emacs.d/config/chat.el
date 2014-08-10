b29bf60338741073e1cefc1fc2330c8555226be7
Jarrell Waggoner <jwaggoner@groupon.com>
2014-08-10 15:34:42 -0500

    add chat back into my emacs config with work settings

---
+++ .emacs.d/config/chat.el
@@ -0,0 +1,15 @@
+(provide 'chat)
+
+(ensure-packages-installed 'jabber)
+
+(setq jabber-account-list
+      '(("jwaggoner@groupon.com"
+	 (:network-server . "talk.google.com")
+	 (:connection-type . ssl))
+	("jarrell.waggoner@gmail.com"
+	 (:network-server . "talk.google.com")
+	 (:connection-type . ssl))))
+
+(setq jabber-chat-buffer-show-avatar nil)
+(setq jabber-show-resources nil)
+(setq jabber-roster-line-format "%c %-25n %u %-8s  %S")
