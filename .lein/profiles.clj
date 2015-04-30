f2c4855d7eed365233ff58a626857966e2a08213
Jarrell Waggoner <malloc47@gmail.com>
2015-04-29 23:23:42 -0500

    upgrade .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,12 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [com.palletops/lein-shorthand "0.4.0"]
+                  [lein-cljfmt "0.1.7"]
+                  [refactor-nrepl "1.0.4"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]
+        :shorthand {. [clojure.repl/apropos
+                       clojure.repl/doc
+                       clojure.pprint/pprint
+                       clojure.repl/pst
+                       clojure.repl/source]}}
+ :auth {:repository-auth {#"nexus" {:username "deployment"
+                                    :password "deployment"}}}}
