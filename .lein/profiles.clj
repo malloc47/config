6ac2bdd4e983c58e1bea583d1a7b4167f6f10ec5
Jarrell Waggoner <jwaggoner@drw.com>
2016-09-22 21:50:04 -0500

    modernize this

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+{:user {:plugins [#_[cider/cider-nrepl "0.9.1"]
                   [com.palletops/lein-shorthand "0.4.0"]
-                  [refactor-nrepl "1.1.0"]
+                  [refactor-nrepl "2.2.0"]
                   [lein-pprint "1.1.1"]]
         :shorthand {. [clojure.repl/apropos
                        clojure.repl/doc
                        clojure.pprint/pprint
                        clojure.repl/pst
-                       clojure.repl/source]}}
- :auth {:repository-auth {#"nexus" {:username "deployment"
-                                    :password "deployment"}}}}
+                       clojure.repl/source]}}}
