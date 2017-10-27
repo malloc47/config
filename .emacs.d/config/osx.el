79d4c76aa99b86c94f7614f1652bf6dad73d645a
Jarrell Waggoner <malloc47@gmail.com>
2017-10-27 11:35:25 -0500

    set path properly in OS X

--- .emacs.d/config/osx.el
+++ .emacs.d/config/osx.el
@@ -1,5 +1,8 @@
 (provide 'osx)
 
+(ensure-packages-installed 'exec-path-from-shell)
+
 (when (eq system-type 'darwin)
   (setq mac-option-modifier nil)
-  (setq mac-command-modifier 'meta))
+  (setq mac-command-modifier 'meta)
+  (exec-path-from-shell-initialize))
