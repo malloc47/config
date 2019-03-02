ccfa17cc706fe5fce2010d4cf8c677c99c35a885
Jarrell Waggoner <malloc47@gmail.com>
2019-03-01 21:48:38 -0600

    fix bugs in users.nix

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
+  ];
 
-   imports = [
-     "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
-   ];
-  
+
   users.users.malloc47 = {
     isNormalUser = true;
     createHome = true;
@@ -14,8 +14,6 @@
     extraGroups = ["wheel" "networkmanager" "audio"];
     uid = 1000;
   };
-
-  users.users.eve.isNormalUser = true;
 
   home-manager.users.malloc47 = import ../config/home.nix ;
 }
