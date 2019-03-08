d40a07db51059745a423b42c2e56c7c1a55f2f94
Jarrell Waggoner <malloc47@gmail.com>
2019-03-08 16:40:07 -0600

    fix settings.nix indentation

--- modules/settings.nix
+++ modules/settings.nix
@@ -1,38 +1,34 @@
- {config, pkgs, lib, ...}:
+{config, pkgs, lib, ...}:
 
- let
-   cfg = config.settings;
- in
+with lib;
 
- with lib;
-
- {
-   options = {
-     settings = {
-       name = mkOption {
-         default = "Jarrell Waggoner";
-         type = with types; uniq string;
-       };
-       username = mkOption {
-         default = "malloc47";
-         type = with types; uniq string;
-       };
-       email = mkOption {
-         default = "malloc47@gmail.com";
-         type = with types; uniq string;
-       };
-       vm = mkOption {
-         type = types.bool;
-         default = false;
-       };
-       terminal = mkOption {
-         default = "alacritty";
-         type = with types; uniq string;
-       };
-       fontSize = mkOption {
-         default = 10;
-         type = types.int;
-       };
-     };
-   };
- }
+{
+  options = {
+    settings = {
+      name = mkOption {
+        default = "Jarrell Waggoner";
+        type = with types; uniq string;
+      };
+      username = mkOption {
+        default = "malloc47";
+        type = with types; uniq string;
+      };
+      email = mkOption {
+        default = "malloc47@gmail.com";
+        type = with types; uniq string;
+      };
+      vm = mkOption {
+        type = types.bool;
+        default = false;
+      };
+      terminal = mkOption {
+        default = "alacritty";
+        type = with types; uniq string;
+      };
+      fontSize = mkOption {
+        default = 12;
+        type = types.int;
+      };
+    };
+  };
+}
