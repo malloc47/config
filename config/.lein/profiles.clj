52a0eeafe9386f261b7a89288ff7db06bea7a9db
Jarrell Waggoner <malloc47@gmail.com>
2019-03-10 17:45:03 -0500

    refactor-nrepl is broken again

--- config/.lein/profiles.clj
+++ config/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [#_[cider/cider-nrepl "0.9.1"]
                   [com.palletops/lein-shorthand "0.4.0"]
-                  [refactor-nrepl "2.2.0"]
+                  #_[refactor-nrepl "2.2.0"]
                   [lein-pprint "1.1.1"]]
         :shorthand {. [clojure.repl/apropos
                        clojure.repl/doc
