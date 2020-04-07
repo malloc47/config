7bf66b4206d9556f16ce3312be970fcccaaf6a94
Jarrell Waggoner <malloc47@gmail.com>
2020-04-06 19:23:15 -0500

    switch from ENSIME to metals + lsp-mode

--- config/.emacs.d/config/scala.el
+++ config/.emacs.d/config/scala.el
@@ -1,3 +1,9 @@
 (provide 'scala)
 
-(ensure-packages-installed 'ensime 'scala-mode 'sbt-mode)
+(ensure-packages-installed 'lsp-mode 'lsp-ui 'company-lsp 'scala-mode 'sbt-mode)
+
+(setq lsp-keymap-prefix "C-c C-l")
+
+(require 'lsp-mode)
+
+(add-hook 'scala-mode-hook #'lsp-deferred)
--- config/.sbt/0.13/plugins/plugins.sbt
+++ config/.sbt/0.13/plugins/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("org.ensime" % "sbt-ensime" % "2.5.1")
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.9.2")
