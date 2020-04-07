eccb198cf369105cc6cc6190f60d873adbaf0dc0
Jarrell Waggoner <malloc47@gmail.com>
2020-04-07 08:50:35 -0400

    switch to native line numbers

--- config/.emacs.d/config/theme.el
+++ config/.emacs.d/config/theme.el
@@ -6,8 +6,6 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-;; show fringe on right for git-gutter-fringe
-(fringe-mode '(0 . 8))
 
 (when (display-graphic-p)
   ;; load color theme
@@ -20,6 +18,4 @@
     (load-theme 'solarized-light t)))
 (add-hook 'after-make-frame-functions 'new-frame-setup)
 
-;; line numbers
-(add-hook 'find-file-hook (lambda () (linum-mode 1)))
-(setq linum-format "%3d ")
+(global-display-line-numbers-mode)
