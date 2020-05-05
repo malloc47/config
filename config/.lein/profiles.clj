d92014138592ea91712eae2158f888588191ec96
Jarrell Waggoner <malloc47@gmail.com>
2020-05-04 20:20:09 -0400

    switch to Gary's updated artifact

--- config/.lein/profiles.clj
+++ config/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[com.palletops/lein-shorthand "0.4.0"]
+{:user {:plugins [[com.gfredericks/lein-shorthand "0.4.1"]
                   [lein-pprint "1.1.1"]]
         :shorthand {. [clojure.repl/apropos
                        clojure.repl/doc
