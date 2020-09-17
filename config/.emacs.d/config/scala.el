01175b2c98496e8f7008b2c7f6d8d8a6912d7d63
Jarrell Waggoner <malloc47@gmail.com>
2020-09-17 10:08:48 -0400

    LSP+metals needs yasnippet for some functionality

--- config/.emacs.d/config/scala.el
+++ config/.emacs.d/config/scala.el
@@ -1,9 +1,19 @@
 (provide 'scala)
 
-(ensure-packages-installed 'lsp-mode 'lsp-ui 'company-lsp 'scala-mode 'sbt-mode)
+(ensure-packages-installed 'lsp-mode 'lsp-ui 'company-lsp 'scala-mode 'sbt-mode 'yasnippet)
 
 (setq lsp-keymap-prefix "C-c C-l")
 
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
+
+(define-key company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common)
+
 (require 'lsp-mode)
+(require 'company-lsp)
+(require 'yasnippet)
+
+(yas-reload-all)
+(add-hook 'scala-mode-hook #'yas-minor-mode)
 
 (add-hook 'scala-mode-hook #'lsp-deferred)
