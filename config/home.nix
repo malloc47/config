47238edcebb6e7e73dc07df99545532fa2fb975f
Jarrell Waggoner <malloc47@gmail.com>
2020-12-06 13:33:44 -0500

    Make zsh work more seamlessly with nix-shell

--- config/home.nix
+++ config/home.nix
@@ -258,6 +258,18 @@ with pkgs.lib;
       "ll" = "ls -al";
       "ns" = "nix-shell --command zsh";
     };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.1.0";
+          sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+        };
+      }
+    ];
     initExtra = let
       cdpath = "$HOME/src" +
         optionalString (config.settings.profile != "malloc47")
