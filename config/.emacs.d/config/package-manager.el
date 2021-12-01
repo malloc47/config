8e40501496edad756ccb419422edd4b902751978
Jarrell Waggoner <malloc47@gmail.com>
2021-12-01 13:15:17 -0500

    RIP marmalade :'(
    https://www.emacswiki.org/emacs/MarmaladeRepo

--- config/.emacs.d/config/package-manager.el
+++ config/.emacs.d/config/package-manager.el
@@ -5,8 +5,6 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
 (add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
