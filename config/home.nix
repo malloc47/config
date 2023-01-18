2ac141e773c05562fefe6449d8f5a934d90a7ab9
Jarrell Waggoner <malloc47@gmail.com>
2023-01-18 12:08:31 -0500

    Stop using mkOptionDefault for i3 keybindings

--- config/home.nix
+++ config/home.nix
@@ -141,7 +141,7 @@ with pkgs.lib;
           '';
         }
       ];
-      keybindings = mkOptionDefault (
+      keybindings = (
         {
           "${mod}+p" = "exec ${pkgs.dmenu}/bin/dmenu_run";
           "${mod}+o" = "exec ${pkgs.clipmenu}/bin/clipmenu";
@@ -172,17 +172,45 @@ with pkgs.lib;
           "${mod}+v" = "nop";
           "${mod}+e" = "nop";
           "${mod}+s" = "nop";
+          "${mod}+Shift+>" = "move workspace to output right";
+          "${mod}+Shift+<" = "move workspace to output left";
+          # Defaults
+          "${mod}+Shift+F" = "floating toggle";
+          "${mod}+f" = "focus mode_toggle";
+          # "${mod}+f" = "fullscreen toggle";
+          "${mod}+a" = "focus parent";
+          "${mod}+1" = "workspace number 1";
+          "${mod}+2" = "workspace number 2";
+          "${mod}+3" = "workspace number 3";
+          "${mod}+4" = "workspace number 4";
+          "${mod}+5" = "workspace number 5";
+          "${mod}+6" = "workspace number 6";
+          "${mod}+7" = "workspace number 7";
+          "${mod}+8" = "workspace number 8";
+          "${mod}+9" = "workspace number 9";
+          "${mod}+0" = "workspace number 10";
+          "${mod}+Shift+1" = "move container to workspace number 1";
+          "${mod}+Shift+2" = "move container to workspace number 2";
+          "${mod}+Shift+3" = "move container to workspace number 3";
+          "${mod}+Shift+4" = "move container to workspace number 4";
+          "${mod}+Shift+5" = "move container to workspace number 5";
+          "${mod}+Shift+6" = "move container to workspace number 6";
+          "${mod}+Shift+7" = "move container to workspace number 7";
+          "${mod}+Shift+8" = "move container to workspace number 8";
+          "${mod}+Shift+9" = "move container to workspace number 9";
+          "${mod}+Shift+0" = "move container to workspace number 10";
+          "${mod}+r" = "mode resize";
         }
         // optionalAttrs (!config.settings.vm)
-        {
-          "${mod}+equal" = "workspace next";
-          "${mod}+minus" = "workspace prev";
-          "${mod}+grave" = "workspace 1";
-          "${mod}+Shift+Control+L" = "exec i3lock";
-          "XF86AudioRaiseVolume" = "exec --no-startup-id amixer sset Master 5%+ unmute";
-          "XF86AudioLowerVolume" = "exec --no-startup-id amixer sset Master 5%- unmute";
-          "XF86AudioMute" = "exec --no-startup-id amixer sset Master toggle";
-        });
+          {
+            "${mod}+equal" = "workspace next";
+            "${mod}+minus" = "workspace prev";
+            "${mod}+grave" = "workspace 1";
+            "${mod}+Shift+Control+L" = "exec i3lock";
+            "XF86AudioRaiseVolume" = "exec --no-startup-id amixer sset Master 5%+ unmute";
+            "XF86AudioLowerVolume" = "exec --no-startup-id amixer sset Master 5%- unmute";
+            "XF86AudioMute" = "exec --no-startup-id amixer sset Master toggle";
+          });
       modes.resize = {
         "h" = "resize shrink width 10 px or 10 ppt";
         "j" = "resize grow height 10 px or 10 ppt";
