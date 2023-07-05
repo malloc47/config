4e857018f6f79c8eaa6b48bbc6d601d47e222aef
Jarrell Waggoner <jwaggoner@drwholdings.com>
2023-07-05 09:57:37 -0400

    Used the wrong key here :-o

--- config/.emacs.d/config/jw-gpt.el
+++ config/.emacs.d/config/jw-gpt.el
@@ -2,14 +2,14 @@
 
 (use-package chatgpt-shell
   :ensure t
-  :custom
+  :config
   (setq chatgpt-shell-openai-key
 	(lambda ()
           (auth-source-pick-first-password :host "api.openai.com"))))
 
 (use-package dall-e-shell
   :ensure t
-  :custom
+  :config
   (setq dall-e-shell-openai-key
 	(lambda ()
           (auth-source-pick-first-password :host "api.openai.com"))))
