46cb31bbc99f1a7ff77b60cae961dbfb72251b73
Jarrell Waggoner <jwaggoner@drwholdings.com>
2023-07-06 21:22:32 -0400

    Add in all API models
    Sourced from:
    
    https://platform.openai.com/docs/models/model-endpoint-compatibility

--- config/.emacs.d/config/jw-gpt.el
+++ config/.emacs.d/config/jw-gpt.el
@@ -5,7 +5,16 @@
   :config
   (setq chatgpt-shell-openai-key
 	(lambda ()
-          (auth-source-pick-first-password :host "api.openai.com"))))
+          (auth-source-pick-first-password :host "api.openai.com")))
+  (setq chatgpt-shell-model-versions
+	'("gpt-3.5-turbo"
+	  "gpt-3.5-turbo-0613"
+	  "gpt-3.5-turbo-16k"
+	  "gpt-3.5-turbo-16k-0613"
+	  "gpt-4"
+	  "gpt-4-0613"
+	  "gpt-4-32k"
+	  "gpt-4-32k-0613")))
 
 (use-package dall-e-shell
   :ensure t
