9a4a090abd2728b3224bdff7c8d8510f437f0f88
Jarrell Waggoner <jwaggoner@drwholdings.com>
2024-06-09 14:48:54 -0400

    Configure corporate chat engine

--- config/.emacs.d/config/jw-gpt.el
+++ config/.emacs.d/config/jw-gpt.el
@@ -5,13 +5,25 @@
   :config
   (setq chatgpt-shell-openai-key
 	(lambda ()
-          (auth-source-pick-first-password :host "api.openai.com")))
+	  (if (string-equal "drw" (system-name))
+	      (auth-source-pick-first-password :host "azure-openai.drwcloud.com")
+	      (auth-source-pick-first-password :host "api.openai.com"))))
+  (when (string-equal "drw" (system-name))
+    (setq chatgpt-shell-api-url-base "https://azure-openai.drwcloud.com")
+    (setq chatgpt-shell-api-url-path "/openai/deployments/gpt-4-turbo/chat/completions?api-version=2024-02-15-preview")
+    (setq chatgpt-shell-auth-header
+	  (lambda ()
+	    (format "api-key: %s"
+		    (auth-source-pick-first-password
+		     :host
+		     "azure-openai.drwcloud.com")))))
   (setq chatgpt-shell-model-versions
 	'("gpt-3.5-turbo"
 	  "gpt-3.5-turbo-0613"
 	  "gpt-3.5-turbo-16k"
 	  "gpt-3.5-turbo-16k-0613"
 	  "gpt-4"
+	  "gpt-4-turbo"
 	  "gpt-4-0613"
 	  "gpt-4-32k"
 	  "gpt-4-32k-0613")))
