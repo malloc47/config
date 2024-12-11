832d47290fac604d1a68bee7e59650e1aa56aa67
Jarrell Waggoner <malloc47@cesare.lan>
2024-12-10 22:26:37 -0500

    refactor into external configuration file

---
+++ darwin/configuration.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }: 
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+  [ 
+    pkgs.vim
+  ];
+ 
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+ 
+  # Enable alternative shell support in nix-darwin.
+  # programs.fish.enable = true;
+ 
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+ 
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+ 
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
+
--- darwin/flake.nix
+++ darwin/flake.nix
@@ -7,37 +7,13 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#cesare
     darwinConfigurations."cesare" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        ./configuration.nix
+      ];
     };
   };
 }
