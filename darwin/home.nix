1d30ac6254ec4fcf69544eee8140efd0f320d50f
Jarrell Waggoner <malloc47@gmail.com>
2025-06-20 16:46:11 -0400

    Add git config to Darwin

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,8 +1,8 @@
-# home.nix
-
 { config, pkgs, ... }:
-
 {
+  imports = [
+    ../modules/settings.nix
+  ];
 
   home = {
     stateVersion = "23.05";
@@ -13,6 +13,25 @@
       pkgs.nixfmt-rfc-style
       rsync
     ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = config.settings.name;
+    userEmail = config.settings.email;
+    aliases = {
+      s = "status -s -uno";
+      gl = "log --oneline --graph";
+    };
+    ignores = [".#*" "*.desktop" "*.lock"];
+    lfs.enable = true;
+    extraConfig = {
+      branch.autosetuprebase = "never";
+      push.default = "simple";
+      core.pager = "less -F -X";
+      pull.ff = "only";
+      init.defaultBranch = "main";
+    };
   };
 
   # Let Home Manager install and manage itself.
