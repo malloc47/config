5609c958c9849d8d09004e5a9456e85e72794180
Jarrell Waggoner <malloc47@gmail.com>
2025-06-21 16:46:26 -0400

    Create activation script to configure OSX terminal

--- darwin/darwin.nix
+++ darwin/darwin.nix
@@ -1,5 +1,9 @@
 { pkgs, config,  ... }:
 {
+  imports = [
+    ../modules/settings.nix
+  ];
+
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToControl = true;
 
@@ -9,5 +13,15 @@
     ApplePressAndHoldEnabled = false;
   };
 
-  # TODO: Use Option as Meta in Terminal app
+  system.activationScripts.extraActivation.text = let
+    user = config.settings.username;
+  in ''
+    # These initializations may be needed, but waiting until something blows up to confirm
+    # sudo -u ${user} -- plutil -insert 'Window Settings' -json '{}' /Users/${user}/Library/Preferences/com.apple.Terminal.plist > /dev/null 2>&1 || true
+    # sudo -u ${user} -- plutil -insert 'Window Settings'.Basic -json '{}' /Users/${user}/Library/Preferences/com.apple.Terminal.plist > /dev/null 2>&1 || true
+    sudo -u ${user} -- plutil -replace 'Window Settings'.Basic.useOptionAsMetaKey -bool YES /Users/${user}/Library/Preferences/com.apple.Terminal.plist
+    sudo -u ${user} -- defaults write com.apple.Terminal "Default Window Settings" -string "Basic"
+  '';
+
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
