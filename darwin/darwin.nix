0c1653184feae383d3206a3820e179c8e650827d
Jarrell Waggoner <malloc47@gmail.com>
2025-06-22 23:03:18 -0400

    Darwin: Setup keyboard shortcuts
    Should probably write a proper plist generator for this, but for now
    just round-trip existing keyboard settings through plist->json->nix and
    snapshot as a file to freeze the current settings.

--- darwin/darwin.nix
+++ darwin/darwin.nix
@@ -4,24 +4,36 @@
     ../modules/settings.nix
   ];
 
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToControl = true;
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
 
-  system.defaults.NSGlobalDomain = {
-    KeyRepeat = 2;
-    InitialKeyRepeat = 25;
-    ApplePressAndHoldEnabled = false;
+    defaults = {
+      NSGlobalDomain = {
+        KeyRepeat = 2;
+        InitialKeyRepeat = 25;
+        ApplePressAndHoldEnabled = false;
+      };
+
+      CustomUserPreferences = {
+        # Run ./keyboard-to-nix.sh to snapshot current keyboard settings
+        "com.apple.symbolichotkeys" = (import ./keyboard.nix);
+      };
+    }; 
+
+    activationScripts.extraActivation.text = let
+      user = config.settings.username;
+    in ''
+      # These initializations may be needed, but waiting until something blows up to confirm
+      # sudo -u ${user} -- plutil -insert 'Window Settings' -json '{}' /Users/${user}/Library/Preferences/com.apple.Terminal.plist > /dev/null 2>&1 || true
+      # sudo -u ${user} -- plutil -insert 'Window Settings'.Basic -json '{}' /Users/${user}/Library/Preferences/com.apple.Terminal.plist > /dev/null 2>&1 || true
+      sudo -u ${user} -- plutil -replace 'Window Settings'.Basic.useOptionAsMetaKey -bool YES /Users/${user}/Library/Preferences/com.apple.Terminal.plist
+      sudo -u ${user} -- defaults write com.apple.Terminal "Default Window Settings" -string "Basic"
+    '';
+
   };
-
-  system.activationScripts.extraActivation.text = let
-    user = config.settings.username;
-  in ''
-    # These initializations may be needed, but waiting until something blows up to confirm
-    # sudo -u ${user} -- plutil -insert 'Window Settings' -json '{}' /Users/${user}/Library/Preferences/com.apple.Terminal.plist > /dev/null 2>&1 || true
-    # sudo -u ${user} -- plutil -insert 'Window Settings'.Basic -json '{}' /Users/${user}/Library/Preferences/com.apple.Terminal.plist > /dev/null 2>&1 || true
-    sudo -u ${user} -- plutil -replace 'Window Settings'.Basic.useOptionAsMetaKey -bool YES /Users/${user}/Library/Preferences/com.apple.Terminal.plist
-    sudo -u ${user} -- defaults write com.apple.Terminal "Default Window Settings" -string "Basic"
-  '';
 
   security.pam.services.sudo_local.touchIdAuth = true;
 }
---
+++ darwin/keyboard.nix
@@ -0,0 +1,227 @@
+{
+  AppleSymbolicHotKeys = {
+    "118" = {
+      enabled = true;
+      value = {
+        parameters = [
+          96
+          50
+          524288
+        ];
+        type = "standard";
+      };
+    };
+    "119" = {
+      enabled = false;
+      value = {
+        parameters = [
+          65535
+          19
+          262144
+        ];
+        type = "standard";
+      };
+    };
+    "15" = {
+      enabled = false;
+    };
+    "16" = {
+      enabled = false;
+    };
+    "160" = {
+      enabled = false;
+      value = {
+        parameters = [
+          65535
+          65535
+          0
+        ];
+        type = "standard";
+      };
+    };
+    "164" = {
+      enabled = false;
+      value = {
+        parameters = [
+          65535
+          65535
+          0
+        ];
+        type = "standard";
+      };
+    };
+    "17" = {
+      enabled = false;
+    };
+    "175" = {
+      enabled = false;
+      value = {
+        parameters = [
+          65535
+          65535
+          0
+        ];
+        type = "standard";
+      };
+    };
+    "18" = {
+      enabled = false;
+    };
+    "184" = {
+      enabled = false;
+      value = {
+        parameters = [
+          53
+          23
+          1179648
+        ];
+        type = "standard";
+      };
+    };
+    "19" = {
+      enabled = false;
+    };
+    "190" = {
+      enabled = false;
+      value = {
+        parameters = [
+          113
+          12
+          8388608
+        ];
+        type = "standard";
+      };
+    };
+    "20" = {
+      enabled = false;
+    };
+    "21" = {
+      enabled = false;
+    };
+    "22" = {
+      enabled = false;
+    };
+    "222" = {
+      enabled = false;
+      value = {
+        parameters = [
+          65535
+          65535
+          0
+        ];
+        type = "standard";
+      };
+    };
+    "23" = {
+      enabled = false;
+    };
+    "24" = {
+      enabled = false;
+    };
+    "25" = {
+      enabled = false;
+    };
+    "26" = {
+      enabled = false;
+    };
+    "52" = {
+      enabled = false;
+      value = {
+        parameters = [
+          100
+          2
+          1572864
+        ];
+        type = "standard";
+      };
+    };
+    "59" = {
+      enabled = false;
+      value = {
+        parameters = [
+          65535
+          96
+          9437184
+        ];
+        type = "standard";
+      };
+    };
+    "60" = {
+      enabled = false;
+      value = {
+        parameters = [
+          32
+          49
+          262144
+        ];
+        type = "standard";
+      };
+    };
+    "61" = {
+      enabled = false;
+      value = {
+        parameters = [
+          32
+          49
+          786432
+        ];
+        type = "standard";
+      };
+    };
+    "79" = {
+      enabled = true;
+      value = {
+        parameters = [
+          45
+          27
+          524288
+        ];
+        type = "standard";
+      };
+    };
+    "80" = {
+      enabled = true;
+      value = {
+        parameters = [
+          45
+          27
+          655360
+        ];
+        type = "standard";
+      };
+    };
+    "81" = {
+      enabled = true;
+      value = {
+        parameters = [
+          61
+          24
+          524288
+        ];
+        type = "standard";
+      };
+    };
+    "82" = {
+      enabled = true;
+      value = {
+        parameters = [
+          61
+          24
+          655360
+        ];
+        type = "standard";
+      };
+    };
+    "98" = {
+      enabled = false;
+      value = {
+        parameters = [
+          47
+          44
+          1179648
+        ];
+        type = "standard";
+      };
+    };
+  };
+}
