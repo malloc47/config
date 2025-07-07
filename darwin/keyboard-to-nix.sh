04c3d70aad36937d036e6a7dd9579a565e58e889
Jarrell Waggoner <malloc47@gmail.com>
2025-07-06 23:09:17 -0400

    darwin: refactor scripts

--- darwin/keyboard-to-nix.sh
+++ darwin/keyboard-to-nix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 FILE=$(mktemp -p $(pwd))
 plutil -convert json -o - ~/Library/Preferences/com.apple.symbolichotkeys.plist | jq . > $FILE
-nix-instantiate --eval -E "builtins.fromJSON (builtins.readFile $FILE)" | nixfmt > keyboard.nix
+nix-instantiate --eval -E "builtins.fromJSON (builtins.readFile $FILE)" | nixfmt
 rm $FILE
