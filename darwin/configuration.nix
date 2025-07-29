8a63e41f01001892db39e782bbd5db1b3871364a
Jarrell Waggoner <jwaggoner@drwholdings.com>
2025-07-28 23:38:17 -0400

    darwin: reduce all OSX motion that I can find

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -35,12 +35,25 @@
         InitialKeyRepeat = 25;
         ApplePressAndHoldEnabled = false;
         NSWindowShouldDragOnGesture = true;
+
+        # Visual optimization
         NSAutomaticWindowAnimationsEnabled = false;
+        NSScrollAnimationEnabled = false;
+        NSWindowResizeTime = .001;
       };
 
       CustomUserPreferences = {
+        NSGlobalDomain  = {
+          NSBrowserColumnAnimationSpeedMultiplier = 0.0;
+          NSScrollViewRubberbanding = 0;
+          QLPanelAnimationDuration = 0.0;
+          NSDocumentRevisionsWindowTransformAnimation = false;
+          NSToolbarFullScreenAnimationDuration = 0.0;
+        };
+
         # Run ./keyboard-to-nix.sh to snapshot current keyboard settings
         "com.apple.symbolichotkeys" = (import ../darwin/keyboard.nix);
+
         "org.nixos.xquartz.X11" = {
           enable_render_extension = 1;
           no_auth = true;
@@ -51,12 +64,29 @@
           no_auth = true;
           nolisten_tcp = false;
         };
+
+        "com.apple.finder".DisableAllAnimations = true;
+
+        "com.apple.dock" = {
+          springboard-show-duration = 0.0;
+          springboard-hide-duration = 0.0;
+          springboard-page-duration = 0.0;
+        };
       };
 
       dock = {
         mru-spaces = false;
         expose-group-apps = true;
+
+        # Disable animations
+        autohide-time-modifier = 0.0;
+        autohide-delay = 0.0;
+        expose-animation-duration = 0.0;
+        mineffect = "scale";
       };
+
+      universalaccess.reduceMotion = true;
+
     };
 
     activationScripts.extraActivation.text = let
